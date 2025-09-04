226d2a6b5fe3b2d5b46da75e39fae9451ccd1a3a
lzhang121 <zhspark@gmail.com>
2025-09-04 23:07:07 +0900

    更新框架结构

---
+++ run.bat
@@ -0,0 +1,26 @@
+
+@echo off
+chcp 65001
+REM 进入项目目录
+cd /d %~dp0
+
+REM 创建虚拟环境
+if not exist venv (
+    python -m venv .venv
+    echo 虚拟环境已创建
+)
+
+REM 激活虚拟环境
+call .venv\Scripts\activate.bat
+echo 虚拟环境已激活
+
+REM 安装依赖
+pip install --upgrade pip
+pip install -r requirements.txt
+
+REM 运行 pytest
+pytest --env=test --alluredir=.\allure-results
+
+REM 启动 Allure 报告
+allure serve .\allure-results
+pause
--- tests/utils/loader.py
+++ tests/utils/loader.py
@@ -4,6 +4,6 @@ import yaml
 
 def load_yaml(file_name: str):
     base_dir = os.path.dirname(os.path.dirname(__file__))
-    file_path = os.path.join(base_dir, "tests/data", file_name)
+    file_path = os.path.join(base_dir,"data", file_name)
     with open(file_path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
